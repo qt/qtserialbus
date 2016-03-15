qt/qtserialbus

commit e40343337940705ceb830f9f20a09084185ab198

Move clearing or updating values into a common function.
Change-Id: I25ee09e5fe70464048500dc4d376d4e38805ca50
Reviewed-by: Alex Blasche <alexander.blasche@theqtcompany.com>

src/serialbus/qmodbusrtuserialmaster.cpp
@@ -97,12 +97,8 @@ bool QModbusRtuSerialMaster::open()
         return true;
 
     Q_D(QModbusRtuSerialMaster);
-
-    d->responseBuffer.clear();
-
-    d->updateSerialPortConnectionInfo();
     if (d->m_serialPort->open(QIODevice::ReadWrite)) {
-        d->m_serialPort->clear();
+        d->setupEnvironment();
         setState(QModbusDevice::ConnectedState);
     } else {
         setError(d->m_serialPort->errorString(), QModbusDevice::ConnectionError);
@@ -119,7 +115,6 @@ void QModbusRtuSerialMaster::close()
         return;
 
     Q_D(QModbusRtuSerialMaster);
-
     if (d->m_serialPort->isOpen())
         d->m_serialPort->close();
 

src/serialbus/qmodbusrtuserialmaster_p.h
@@ -218,32 +218,38 @@ public:
             Q_Q(QModbusRtuSerialMaster);
             if (q->state() != QModbusDevice::ClosingState)
                 q->close();
+            m_sendTimer.stop();
+            m_responseTimer.stop();
         });
     }
 
-    void updateSerialPortConnectionInfo() {
+    void setupEnvironment() {
         if (m_serialPort) {
+            m_serialPort->clear();
             m_serialPort->setPortName(m_comPort);
             m_serialPort->setParity(m_parity);
             m_serialPort->setBaudRate(m_baudRate);
             m_serialPort->setDataBits(m_dataBits);
             m_serialPort->setStopBits(m_stopBits);
-
-            // According to the Modbus specification, in RTU mode message frames
-            // are separated by a silent interval of at least 3.5 character times.
-            // Calculate the timeout if we are less than 19200 baud, use a fixed
-            // timeout for everything equal or greater than 19200 baud.
-            if (m_baudRate < 19200) {
-                // Example: 9600 baud, 11 bit per packet -> 872 char/sec
-                // so: 1000 ms / 872 char = 1.147 ms/char * 3.5 character
-                // Always round up because the spec requests at least 3.5 char.
-                m_timeoutThreeDotFiveMs = qCeil(3500. / (qreal(m_baudRate) / 11.));
-            } else {
-                // The spec recommends a timeout value of 1.750 msec. Without such
-                // precise single-shot timers use a approximated value of 1.750 msec.
-                m_timeoutThreeDotFiveMs = 2;
-            }
         }
+
+        // According to the Modbus specification, in RTU mode message frames
+        // are separated by a silent interval of at least 3.5 character times.
+        // Calculate the timeout if we are less than 19200 baud, use a fixed
+        // timeout for everything equal or greater than 19200 baud.
+        if (m_baudRate < 19200) {
+            // Example: 9600 baud, 11 bit per packet -> 872 char/sec
+            // so: 1000 ms / 872 char = 1.147 ms/char * 3.5 character
+            // Always round up because the spec requests at least 3.5 char.
+            m_timeoutThreeDotFiveMs = qCeil(3500. / (qreal(m_baudRate) / 11.));
+        } else {
+            // The spec recommends a timeout value of 1.750 msec. Without such
+            // precise single-shot timers use a approximated value of 1.750 msec.
+            m_timeoutThreeDotFiveMs = 2;
+        }
+
+        responseBuffer.clear();
+        m_state = QModbusRtuSerialMasterPrivate::Idle;
     }
 
     void scheduleNextRequest() {

src/serialbus/qmodbusrtuserialslave.cpp
@@ -107,9 +107,8 @@ bool QModbusRtuSerialSlave::open()
         return true;
 
     Q_D(QModbusRtuSerialSlave);
-    d->updateSerialPortConnectionInfo();
     if (d->m_serialPort->open(QIODevice::ReadWrite)) {
-        d->m_serialPort->clear();
+        d->setupEnvironment();
         setState(QModbusDevice::ConnectedState);
     } else {
         setError(d->m_serialPort->errorString(), QModbusDevice::ConnectionError);
@@ -126,7 +125,6 @@ void QModbusRtuSerialSlave::close()
         return;
 
     Q_D(QModbusRtuSerialSlave);
-
     if (d->m_serialPort->isOpen())
         d->m_serialPort->close();
 

src/serialbus/qmodbusrtuserialslave_p.h
@@ -321,14 +321,17 @@ public:
         });
     }
 
-    void updateSerialPortConnectionInfo() {
+    void setupEnvironment() {
         if (m_serialPort) {
+            m_serialPort->clear();
             m_serialPort->setPortName(m_comPort);
             m_serialPort->setParity(m_parity);
             m_serialPort->setBaudRate(m_baudRate);
             m_serialPort->setDataBits(m_dataBits);
             m_serialPort->setStopBits(m_stopBits);
         }
+
+        m_requestBuffer.clear();
     }
 
     QByteArray m_requestBuffer;;

commit ed4f07584064f0cae3321cdec5272c85912a90e3

Fix serial line access order. Unbreaks serial line connect.
Fix issues introduced by commit e40343337940705ceb830f9f20a:
 * Setup the serial line environment before open, calls after
   open will not change the the values until reconnect.
 * Calling clear on an closed port will set and emit an error,
   successive calls to open() will fail because of how the error
   signal is received. Even though the error was set before the
   successful open, the signal is delivered after the open call
   and and so we close the connection again.

Change-Id: Ic694e9d144c24bf94c4e94e5483aaddcc4c664a5
Reviewed-by: Alex Blasche <alexander.blasche@theqtcompany.com>

src/serialbus/qmodbusrtuserialmaster.cpp
@@ -97,9 +97,10 @@ bool QModbusRtuSerialMaster::open()
         return true;
 
     Q_D(QModbusRtuSerialMaster);
+    d->setupEnvironment(); // to be done before open
     if (d->m_serialPort->open(QIODevice::ReadWrite)) {
-        d->setupEnvironment();
         setState(QModbusDevice::ConnectedState);
+        d->m_serialPort->clear(); // only possible after open
     } else {
         setError(d->m_serialPort->errorString(), QModbusDevice::ConnectionError);
     }

src/serialbus/qmodbusrtuserialmaster_p.h
@@ -225,7 +225,6 @@ public:
 
     void setupEnvironment() {
         if (m_serialPort) {
-            m_serialPort->clear();
             m_serialPort->setPortName(m_comPort);
             m_serialPort->setParity(m_parity);
             m_serialPort->setBaudRate(m_baudRate);

src/serialbus/qmodbusrtuserialslave.cpp
@@ -107,9 +107,10 @@ bool QModbusRtuSerialSlave::open()
         return true;
 
     Q_D(QModbusRtuSerialSlave);
+    d->setupEnvironment(); // to be done before open
     if (d->m_serialPort->open(QIODevice::ReadWrite)) {
-        d->setupEnvironment();
         setState(QModbusDevice::ConnectedState);
+        d->m_serialPort->clear(); // only possible after open
     } else {
         setError(d->m_serialPort->errorString(), QModbusDevice::ConnectionError);
     }

src/serialbus/qmodbusrtuserialslave_p.h
@@ -323,7 +323,6 @@ public:
 
     void setupEnvironment() {
         if (m_serialPort) {
-            m_serialPort->clear();
             m_serialPort->setPortName(m_comPort);
             m_serialPort->setParity(m_parity);
             m_serialPort->setBaudRate(m_baudRate);