eec4d3034e4f1371fe60728e3f84bcb8ddd32c93
Andre Hartmann <aha_1980@gmx.de>
2017-11-11 19:58:16 +0100

    SocketCAN: Compile with Kernels without CAN FD BRS/ESI
    [ChangeLog][SocketCAN] Fixed compiling the SocketCAN plugin
    with older Kernels without support for CAN FD bitrate switch
    and error state indicator.
    
    Task-number: QTBUG-64406
    Change-Id: I05a7869b7df64ae6c3c29aa69dbf423d886b610a
    Reviewed-by: Denis Shienkov <denis.shienkov@gmail.com>
    Reviewed-by: Alex Blasche <alexander.blasche@qt.io>

--- src/plugins/canbus/socketcan/socketcanbackend.cpp
+++ src/plugins/canbus/socketcan/socketcanbackend.cpp
@@ -74,6 +74,13 @@ struct canfd_frame {
 
 #endif
 
+#ifndef CANFD_BRS
+#   define CANFD_BRS 0x01 /* bit rate switch (second bitrate for payload data) */
+#endif
+#ifndef CANFD_ESI
+#   define CANFD_ESI 0x02 /* error state indicator of the transmitting node */
+#endif
+
 QT_BEGIN_NAMESPACE
 
 const char sysClassNetC[] = "/sys/class/net/";
