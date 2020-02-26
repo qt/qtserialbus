6e275b20911fc3e5af2a1b972ec3550235250157
Andre Hartmann <aha_1980@gmx.de>
2020-02-26 11:22:09 +0100

    PeakCAN: Fix Clang warning on Windows
    GetLastError() return DWORD aka unsigned long,
    while qt_error_string takes an int.
    
    Change-Id: Ie0c543645c149a9714cdedfdd2ec39b5959fedb8
    Reviewed-by: Alex Blasche <alexander.blasche@qt.io>

--- src/plugins/canbus/peakcan/peakcanbackend.cpp
+++ src/plugins/canbus/peakcan/peakcanbackend.cpp
@@ -344,7 +344,7 @@ bool PeakCanBackendPrivate::open()
     if (readHandle == INVALID_HANDLE_VALUE) {
         readHandle = ::CreateEvent(nullptr, FALSE, FALSE, nullptr);
         if (Q_UNLIKELY(!readHandle)) {
-            const QString errorString = qt_error_string(::GetLastError());
+            const QString errorString = qt_error_string(int(::GetLastError()));
             qCCritical(QT_CANBUS_PLUGINS_PEAKCAN, "Cannot create receive event handler: %ls",
                        qUtf16Printable(errorString));
             q->setError(errorString, QCanBusDevice::ConnectionError);
@@ -404,7 +404,7 @@ void PeakCanBackendPrivate::close()
 
 #if defined(Q_OS_WIN32)
     if (readHandle && (readHandle != INVALID_HANDLE_VALUE)) {
-        const QString errorString = qt_error_string(::GetLastError());
+        const QString errorString = qt_error_string(int(::GetLastError()));
         if (Q_UNLIKELY(!::CloseHandle(readHandle))) {
             qCCritical(QT_CANBUS_PLUGINS_PEAKCAN, "Cannot close read handle: %ls",
                        qUtf16Printable(errorString));
