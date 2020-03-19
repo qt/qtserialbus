qt/qtserialbus

commit e23f4ced90381925406bd854dd3b3d1eeb478d87

QCanBusFrame: Introduce local echo flag
Inspiration for change after reading the SocketCAN mailing list thread:
https://marc.info/?l=linux-can&m=147438437202356&w=2

The main usage is, to verify at application stage, that and when a
frame was really sent on the bus. For architectures like SocketCAN,
where multiple applications can connect to one CAN bus on the same
system, this flag also indicates frames that are sent by an other
local running application (i.e. not coming from the real bus).

Implementations done for SystecCAN, VectorCAN and SocketCAN plugin.
PCAN Basic does not seem to have support for this flag.

For SocketCAN, see section "4.1.7 RAW socket returned message flags"
in https://www.kernel.org/doc/Documentation/networking/can.txt
for reference.

[ChangeLog][QCanBusFrame] Added local echo flag to mark all echo frames
that are generated by the hardware on successful transmission to CAN bus
as such.

Change-Id: I66d4ce1ec6dfd0a0b994a7b7a9de1a5f503fcb70
Reviewed-by: Denis Shienkov <denis.shienkov@gmail.com>
Reviewed-by: Alex Blasche <alexander.blasche@qt.io>

src/plugins/canbus/socketcan/socketcanbackend.cpp
@@ -325,7 +325,6 @@ bool SocketCanBackend::applyConfigurationParameter(int key, const QVariant &valu
 
 bool SocketCanBackend::connectSocket()
 {
-    struct sockaddr_can address;
     struct ifreq interface;
 
     if (Q_UNLIKELY((canSocket = socket(PF_CAN, SOCK_RAW | SOCK_NONBLOCK, CAN_RAW)) < 0)) {
@@ -341,14 +340,20 @@ bool SocketCanBackend::connectSocket()
         return false;
     }
 
-    address.can_family  = AF_CAN;
-    address.can_ifindex = interface.ifr_ifindex;
+    m_address.can_family  = AF_CAN;
+    m_address.can_ifindex = interface.ifr_ifindex;
 
-    if (Q_UNLIKELY(bind(canSocket, reinterpret_cast<struct sockaddr *>(&address), sizeof(address)) < 0)) {
+    if (Q_UNLIKELY(bind(canSocket, reinterpret_cast<struct sockaddr *>(&m_address), sizeof(m_address)) < 0)) {
         setError(qt_error_string(errno),
                  QCanBusDevice::CanBusError::ConnectionError);
         return false;
     }
+
+    m_iov.iov_base = &m_frame;
+    m_msg.msg_name = &m_address;
+    m_msg.msg_iov = &m_iov;
+    m_msg.msg_iovlen = 1;
+    m_msg.msg_control = &m_ctrlmsg;
 
     delete notifier;
 
@@ -637,10 +642,13 @@ void SocketCanBackend::readSocket()
     QVector<QCanBusFrame> newFrames;
 
     for (;;) {
-        struct canfd_frame frame;
-        int bytesReceived;
+        memset(&m_frame, 0, sizeof(m_frame));
+        m_iov.iov_len = sizeof(m_frame);
+        m_msg.msg_namelen = sizeof(m_addr);
+        m_msg.msg_controllen = sizeof(m_ctrlmsg);
+        m_msg.msg_flags = 0;
 
-        bytesReceived = ::read(canSocket, &frame, sizeof(frame));
+        const int bytesReceived = ::recvmsg(canSocket, &m_msg, 0);
 
         if (bytesReceived <= 0) {
             break;
@@ -648,7 +656,7 @@ void SocketCanBackend::readSocket()
             setError(tr("ERROR SocketCanBackend: incomplete CAN frame"),
                      QCanBusDevice::CanBusError::ReadError);
             continue;
-        } else if (Q_UNLIKELY(frame.len > bytesReceived - offsetof(canfd_frame, data))) {
+        } else if (Q_UNLIKELY(m_frame.len > bytesReceived - offsetof(canfd_frame, data))) {
             setError(tr("ERROR SocketCanBackend: invalid CAN frame length"),
                      QCanBusDevice::CanBusError::ReadError);
             continue;
@@ -666,21 +674,23 @@ void SocketCanBackend::readSocket()
         bufferedFrame.setTimeStamp(stamp);
         bufferedFrame.setFlexibleDataRateFormat(bytesReceived == CANFD_MTU);
 
-        bufferedFrame.setExtendedFrameFormat(frame.can_id & CAN_EFF_FLAG);
-        Q_ASSERT(frame.len <= CANFD_MAX_DLEN);
+        bufferedFrame.setExtendedFrameFormat(m_frame.can_id & CAN_EFF_FLAG);
+        Q_ASSERT(m_frame.len <= CANFD_MAX_DLEN);
 
-        if (frame.can_id & CAN_RTR_FLAG)
+        if (m_frame.can_id & CAN_RTR_FLAG)
             bufferedFrame.setFrameType(QCanBusFrame::RemoteRequestFrame);
-        if (frame.can_id & CAN_ERR_FLAG)
+        if (m_frame.can_id & CAN_ERR_FLAG)
             bufferedFrame.setFrameType(QCanBusFrame::ErrorFrame);
-        if (frame.flags & CANFD_BRS)
+        if (m_frame.flags & CANFD_BRS)
             bufferedFrame.setBitrateSwitch(true);
-        if (frame.flags & CANFD_ESI)
+        if (m_frame.flags & CANFD_ESI)
             bufferedFrame.setErrorStateIndicator(true);
+        if (m_msg.msg_flags & MSG_CONFIRM)
+            bufferedFrame.setLocalEcho(true);
 
-        bufferedFrame.setFrameId(frame.can_id & CAN_EFF_MASK);
+        bufferedFrame.setFrameId(m_frame.can_id & CAN_EFF_MASK);
 
-        const QByteArray load(reinterpret_cast<char *>(frame.data), frame.len);
+        const QByteArray load(reinterpret_cast<char *>(m_frame.data), m_frame.len);
         bufferedFrame.setPayload(load);
 
         newFrames.append(std::move(bufferedFrame));

src/plugins/canbus/socketcan/socketcanbackend.h
@@ -45,6 +45,12 @@
 #include <QtCore/qstring.h>
 #include <QtCore/qvariant.h>
 
+// The order of the following includes is mandatory, because some
+// distributions use sa_family_t in can.h without including socket.h
+#include <sys/socket.h>
+#include <sys/uio.h>
+#include <linux/can.h>
+
 QT_BEGIN_NAMESPACE
 
 class SocketCanBackend : public QCanBusDevice
@@ -72,6 +78,13 @@ private:
     void resetConfigurations();
     bool connectSocket();
     bool applyConfigurationParameter(int key, const QVariant &value);
+
+    canfd_frame m_frame;
+    sockaddr_can m_address;
+    msghdr m_msg;
+    iovec m_iov;
+    sockaddr_can m_addr;
+    char m_ctrlmsg[CMSG_SPACE(sizeof(timeval)) + CMSG_SPACE(sizeof(__u32))];
 
     qint64 canSocket = -1;
     QSocketNotifier *notifier = nullptr;

examples/serialbus/can/mainwindow.cpp
@@ -190,13 +190,16 @@ void MainWindow::closeEvent(QCloseEvent *event)
 
 static QString frameFlags(const QCanBusFrame &frame)
 {
-    if (frame.hasBitrateSwitch() && frame.hasErrorStateIndicator())
-        return QStringLiteral(" B E ");
+    QString result = QLatin1String(" --- ");
+
     if (frame.hasBitrateSwitch())
-        return QStringLiteral(" B - ");
+        result[1] = QLatin1Char('B');
     if (frame.hasErrorStateIndicator())
-        return QStringLiteral(" - E ");
-    return QStringLiteral(" - - ");
+        result[2] = QLatin1Char('E');
+    if (frame.hasLocalEcho())
+        result[3] = QLatin1Char('L');
+
+    return result;
 }
 
 void MainWindow::checkMessages()

src/plugins/canbus/systeccan/systeccanbackend.cpp
@@ -394,6 +394,7 @@ void SystecCanBackendPrivate::readAllReceivedMessages()
         // TODO: Timestamp can also be set to 100 us resolution with kUcanModeHighResTimer
         frame.setTimeStamp(QCanBusFrame::TimeStamp::fromMicroSeconds(message.m_dwTime * 1000));
         frame.setExtendedFrameFormat(message.m_bFF & USBCAN_MSG_FF_EXT);
+        frame.setLocalEcho(message.m_bFF & USBCAN_MSG_FF_ECHO);
         frame.setFrameType((message.m_bFF & USBCAN_MSG_FF_RTR)
                            ? QCanBusFrame::RemoteRequestFrame
                            : QCanBusFrame::DataFrame);

src/plugins/canbus/vectorcan/vectorcanbackend.cpp
@@ -359,6 +359,7 @@ void VectorCanBackendPrivate::startRead()
                            QByteArray(reinterpret_cast<const char *>(msg.data), int(msg.dlc)));
         frame.setTimeStamp(QCanBusFrame::TimeStamp::fromMicroSeconds(event.timeStamp / 1000));
         frame.setExtendedFrameFormat(msg.id & XL_CAN_EXT_MSG_ID);
+        frame.setLocalEcho(msg.flags & XL_CAN_MSG_FLAG_TX_COMPLETED);
         frame.setFrameType((msg.flags & XL_CAN_MSG_FLAG_REMOTE_FRAME)
                            ? QCanBusFrame::RemoteRequestFrame
                            : (msg.flags & XL_CAN_MSG_FLAG_ERROR_FRAME)

src/serialbus/doc/src/socketcan.qdoc
@@ -149,12 +149,15 @@
             \li To meet the multiple-user needs, the local loopback is enabled by default.
                 This means, whenever a CAN frame is transmitted on the CAN bus, a local
                 echo of this frame is sent to all applications connected to this CAN device.
+                If this option is enabled, the therefore received frames are marked with
+                QCanBusFrame::hasLocalEcho()
         \row
             \li QCanBusDevice::ReceiveOwnKey
             \li The reception of the CAN frames on the same socket that was sending the CAN frame
                 is disabled by default. When enabling this option, all CAN frames sent to the CAN
                 bus immediately appear in the receive buffer. This can be used to check if sending
-                was successful.
+                was successful. If this option is enabled, the therefore received frames are marked
+                with QCanBusFrame::hasLocalEcho()
         \row
             \li QCanBusDevice::ErrorFilterKey
             \li A CAN interface driver can generate so called \e {Error Message Frames} that can

src/serialbus/doc/src/systeccan.qdoc
@@ -98,6 +98,7 @@
         \row
             \li QCanBusDevice::ReceiveOwnKey
             \li The reception of CAN frames on the same channel that was sending the CAN frame
-                is disabled by default.
+                is disabled by default. If this option is enabled, the therefore received frames
+                are marked with QCanBusFrame::hasLocalEcho()
    \endtable
 */

src/serialbus/doc/src/vectorcan.qdoc
@@ -96,6 +96,7 @@
             \li The reception of the CAN frames on the same device that was sending the CAN frame
                 is disabled by default. When enabling this option, all CAN frames sent to the CAN
                 bus immediately appear in the receive buffer. This can be used to check if sending
-                was successful.
+                was successful. If this option is enabled, the therefore received frames are marked
+                with QCanBusFrame::hasLocalEcho()
    \endtable
 */

commit 16817cba8720797f20c6d8e638614356d909529b

SocketCAN: Fix compile with old Linux Kernels
qtserialbus/src/plugins/canbus/socketcan/socketcanbackend.h:91:5:
error: ‘canfd_frame’ does not name a type

That was already fixed earlier in 96d291cb,
but broken again by e23f4ced.

Fixes: QTBUG-82814
Change-Id: I8bfc4168165cf8e266b89e8327f88f862cf1b348
Reviewed-by: Denis Shienkov <denis.shienkov@gmail.com>
Reviewed-by: Rolf Eike Beer <eb@emlix.com>
Reviewed-by: Alex Blasche <alexander.blasche@qt.io>

src/plugins/canbus/socketcan/socketcanbackend.cpp
@@ -56,30 +56,6 @@
 #include <sys/ioctl.h>
 #include <sys/time.h>
 
-#ifndef CANFD_MTU
-// CAN FD support was added by Linux kernel 3.6
-// For prior kernels we redefine the missing defines here
-// they are taken from linux/can/raw.h & linux/can.h
-
-enum {
-    CAN_RAW_FD_FRAMES = 5
-};
-
-#define CAN_MAX_DLEN 8
-#define CANFD_MAX_DLEN 64
-struct canfd_frame {
-    canid_t can_id;  /* 32 bit CAN_ID + EFF/RTR/ERR flags */
-    __u8    len;     /* frame payload length in byte */
-    __u8    flags;   /* additional flags for CAN FD */
-    __u8    __res0;  /* reserved / padding */
-    __u8    __res1;  /* reserved / padding */
-    __u8    data[CANFD_MAX_DLEN] __attribute__((aligned(8)));
-};
-#define CAN_MTU     (sizeof(struct can_frame))
-#define CANFD_MTU   (sizeof(struct canfd_frame))
-
-#endif
-
 #ifndef CANFD_BRS
 #   define CANFD_BRS 0x01 /* bit rate switch (second bitrate for payload data) */
 #endif

src/plugins/canbus/socketcan/socketcanbackend.h
@@ -54,6 +54,30 @@
 
 #include <memory>
 
+#ifndef CANFD_MTU
+// CAN FD support was added by Linux kernel 3.6
+// For prior kernels we redefine the missing defines here
+// they are taken from linux/can/raw.h & linux/can.h
+
+enum {
+    CAN_RAW_FD_FRAMES = 5
+};
+
+#define CAN_MAX_DLEN 8
+#define CANFD_MAX_DLEN 64
+struct canfd_frame {
+    canid_t can_id;  /* 32 bit CAN_ID + EFF/RTR/ERR flags */
+    __u8    len;     /* frame payload length in byte */
+    __u8    flags;   /* additional flags for CAN FD */
+    __u8    __res0;  /* reserved / padding */
+    __u8    __res1;  /* reserved / padding */
+    __u8    data[CANFD_MAX_DLEN] __attribute__((aligned(8)));
+};
+#define CAN_MTU     (sizeof(struct can_frame))
+#define CANFD_MTU   (sizeof(struct canfd_frame))
+
+#endif
+
 QT_BEGIN_NAMESPACE
 
 class LibSocketCan;