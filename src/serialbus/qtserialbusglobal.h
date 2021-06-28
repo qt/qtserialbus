44bd0f48cf2ac1daf2e268866e3592f0cc85918d
Alexey Edelev <alexey.edelev@qt.io>
2021-06-28 16:25:12 +0200

    Migrate to autogenerated cpp exports
    Replace the hardcoded cpp exports with a generated one where it's
    applicable.
    
    Task-number: QTBUG-90492
    Change-Id: I6f5b1c6e02948f3624ff1fd7a0bdf2cfac4c293b
    Reviewed-by: Qt CI Bot <qt_ci_bot@qt-project.org>
    Reviewed-by: Joerg Bornemann <joerg.bornemann@qt.io>

--- src/serialbus/qtserialbusglobal.h
+++ src/serialbus/qtserialbusglobal.h
@@ -39,19 +39,6 @@
 
 #include <QtCore/qglobal.h>
 #include <QtSerialBus/qtserialbus-config.h>
-
-QT_BEGIN_NAMESPACE
-
-#ifndef QT_STATIC
-#  if defined(QT_BUILD_SERIALBUS_LIB)
-#    define Q_SERIALBUS_EXPORT Q_DECL_EXPORT
-#  else
-#    define Q_SERIALBUS_EXPORT Q_DECL_IMPORT
-#  endif
-#else
-#  define Q_SERIALBUS_EXPORT
-#endif
-
-QT_END_NAMESPACE
+#include <QtSerialBus/qtserialbusexports.h>
 
 #endif // QTSERIALBUSGLOBAL_H
