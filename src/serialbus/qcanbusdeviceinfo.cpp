eb5fb86d95494d2fe3b96bddfb2c0718120c756f
Ivan Solovev <ivan.solovev@qt.io>
2025-06-18 09:23:34 +0200

    QCanBusDeviceInfo: implement move-ctor properly
    It should be noexcept and inline (ideally, = default).
    We can do it by using the QT_{DECLARE,DEFINE}_QSDP_SPECIALIZATION_DTOR
    macros to provide the proper d-tor specialization for the Private class.
    
    Amends 90b4fe4861a9963ddb239833c34cf50ed32078ec.
    
    Found in Qt 6.10 API review.
    
    Task-number: QTBUG-137478
    Pick-to: 6.10
    Change-Id: I9015afbaf174cb113d8fd0c0fc0f8764ab78dc9b
    Reviewed-by: Marc Mutz <marc.mutz@qt.io>

--- src/serialbus/qcanbusdeviceinfo.cpp
+++ src/serialbus/qcanbusdeviceinfo.cpp
@@ -6,6 +6,8 @@
 
 QT_BEGIN_NAMESPACE
 
+QT_DEFINE_QSDP_SPECIALIZATION_DTOR(QCanBusDeviceInfoPrivate)
+
 /*!
     \class QCanBusDeviceInfo
     \inmodule QtSerialBus
@@ -23,13 +25,11 @@ QT_BEGIN_NAMESPACE
 QCanBusDeviceInfo::QCanBusDeviceInfo(const QCanBusDeviceInfo &) = default;
 
 /*!
+    \fn QCanBusDeviceInfo::QCanBusDeviceInfo(QCanBusDeviceInfo &&other)
+
     Move-constructs a CAN bus device info from \a other.
     \since 6.10
 */
-QCanBusDeviceInfo::QCanBusDeviceInfo(QCanBusDeviceInfo &&other)
-    : d_ptr(std::move(other.d_ptr))
-{
-}
 
 /*!
     Constructs a CAN bus device info from QCanBusDeviceInfoPrivate \a dd.
