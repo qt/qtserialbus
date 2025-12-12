e8b13370ced5ea10a4d99289fa3b214fdc2cc34d
Fabian Kosmale <fabian.kosmale@qt.io>
2025-12-12 12:46:30 +0100

    QCanBusFrame: Apply Q_ENUM to the FrameError enum again
    d772275d278a333486d87e8046444bc21afe6cd0 removed it when porting Q_FLAGS
    to Q_FLAG. While this shouldn't matter for the QMetaObject, it matters
    for other meta-type interactions with the enum itself (notably
    QMetaEnum::fromType, which no longer worked). Fix this by adding Q_ENUM
    once more.
    
    Pick-to: 6.10
    Change-Id: I8031be8066ae14db7525a8b9f75c017e671f03fc
    Reviewed-by: Marc Mutz <marc.mutz@qt.io>
    Reviewed-by: André Hartmann <aha_1980@gmx.de>
    Reviewed-by: Ahmad Samir <a.samirh78@gmail.com>
    (cherry picked from commit 4a8686587e96ca66e328c10d2eaeb82e922b183a)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/serialbus/qcanbusframe.h
+++ src/serialbus/qcanbusframe.h
@@ -75,6 +75,7 @@ public:
         //only 29 bits usable
     };
     Q_DECLARE_FLAGS(FrameErrors, FrameError)
+    Q_ENUM(FrameError)
     Q_FLAG(FrameErrors)
 
     explicit QCanBusFrame(QCanBusFrame::FrameId identifier, const QByteArray &data) :
