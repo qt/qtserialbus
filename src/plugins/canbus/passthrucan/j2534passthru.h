9195f056e3b654a0d759109ae9cf36729d53b565
Volker Hilsheimer <volker.hilsheimer@qt.io>
2026-02-06 14:08:38 +0100

    QUIP-23: mark remaining files as not security critical
    Previous changes, such as 85439f5be2c9ebfa38436b6076cfe6be2fc839e4,
    marked critical sources as such. The remaining sources do not provide
    an interface through which data crosses trust boundaries, so we can mark
    them with the default security classification.
    
    Task-number: QTBUG-135465
    Pick-to: 6.11 6.10 6.8
    Change-Id: Ic6b0db1136584618917a2315792af4b5762ef4fd
    Reviewed-by: Ivan Solovev <ivan.solovev@qt.io>

--- src/plugins/canbus/passthrucan/j2534passthru.h
+++ src/plugins/canbus/passthrucan/j2534passthru.h
@@ -1,5 +1,6 @@
 // Copyright (C) 2017 Ford Motor Company.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+// Qt-Security score:significant reason:default
 
 #ifndef PASSTHRUCAN_J2534PASSTHRU_H
 #define PASSTHRUCAN_J2534PASSTHRU_H
